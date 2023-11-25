8e6ee349f42eac00e7f7aa946aabbc90cf0985ca
RifkiSstwnn <rifkincorp@gmail.com>
2023-11-25 11:00:28 +0700

    data akun solved error

--- admin/module/akun/index.php
+++ admin/module/akun/index.php
@@ -7,7 +7,7 @@
 
         <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
             <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                <h1 class="h2">Data Admin</h1>
+                <h1 class="h2">Data Akun</h1>
             </div>
             <div class="row">
                 <?php
@@ -25,13 +25,17 @@
                         <tbody>
                         <?php
                          // Mengambil dan menampilkan data admin
-                        $id = antiinjection($koneksi, $_POST['user_id']);
-                        $query = "SELECT t.nama_teknisi AS nama , t.nip AS nip,t.jk as jk, u.username AS username, u.email AS email FROM user AS u 
-                                INNER JOIN teknisi AS t ON t.nip = u.unicode WHERE u.level = 'Teknisi' AND u.user_id = '$id';" ;
-                        $reqult = mysqli_query($koneksi, $query);
-                        while ($row = mysqli_fetch_assoc($reqult)) {
+                         $username = $_SESSION['username'];
+                         $id = "SELECT user_id FROM user where username='$username'";
+                         $id_result = mysqli_query($koneksi, $id);
+                         // Mengambil nilai user_id dari objek mysqli_result
+                         $id_row = mysqli_fetch_assoc($id_result);
+                         $id_session = $id_row['user_id'];
+                         $query = "SELECT t.nama_teknisi AS nama , t.nip, t.jk, u.username, u.email FROM user AS u 
+                                   INNER JOIN teknisi AS t ON t.nip = u.unicode WHERE u.level = 'Teknisi' AND u.user_id = $id_session;" ;
+                         $result = mysqli_query($koneksi, $query);
+                        while ($row = mysqli_fetch_assoc($result)) {
                         ?>
-                            <li class="user-item">
                                 <strong>Nama: </strong><?= $row['nama'] ?><br>
                                 <strong>unicode: </strong><?= $row['nip'] ?><br>
                                 <strong>Username: </strong><?= $row['username'] ?><br>
@@ -39,11 +43,12 @@
                                 <strong>Jenis Kelamin: </strong><?= $row['jk'] ?><br>
                                 <button type="button" class="btn btn-warning btn-xs"><i class="fa fa-pencil-square" aria-hidden="true"></i> Edit</button>
                                 <button type="button" class="btn btn-danger btn-xs" onclick="javascript:return confirm('Hapus Data Jabatan ?');"><i class="fa fa-trash-o" aria-hidden="true"></i> Hapus</button>
-                                </li>
                             <?php } ?>
                         </tbody>
                     </table>
                 </div>
             </main>
+    </div>
+</div>
 
 
