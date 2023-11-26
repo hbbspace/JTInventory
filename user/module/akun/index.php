a9bf19d5bbbcd6f45dd609f8a94002242c227585
mulkihakim <mulkihkm@gmail.com>
2023-11-26 21:48:37 +0700

    tampilan fitur akun user

--- user/module/akun/index.php
+++ user/module/akun/index.php
@@ -16,7 +16,7 @@ tr{
     <div class="row">
         <?php
         // Include template menu
-        include "admin/template/menu.php";
+        include "user/template/menu.php";
         ?>
 
         <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
@@ -39,8 +39,18 @@ tr{
                             <?php
                             //mengambil id dengan mengguakan session user_id
                             $id = $_SESSION['user_id'];
-                            $query = "SELECT t.nama_teknisi AS nama , t.nip, t.jk, u.username, u.email FROM user AS u 
-                                    INNER JOIN teknisi AS t ON t.nip = u.unicode WHERE u.level = 'Teknisi' AND u.user_id = '$id'"; 
+                            $userLevel = $_SESSION['level'];
+                            if ($userLevel === 'Mahasiswa') {
+                                $query = "SELECT m.nama_mhs AS nama , m.nim, m.jk, u.username, u.email 
+                                          FROM user AS u 
+                                          INNER JOIN mahasiswa AS m ON m.nim = u.unicode 
+                                          WHERE u.level = 'Mahasiswa' AND u.user_id = '$id'";
+                            } elseif ($userLevel === 'Dosen') {
+                                $query = "SELECT d.nama_dosen AS nama, d.nidn, d.jk, u.username, u.email 
+                                          FROM user AS u 
+                                          INNER JOIN dosen AS d ON d.nidn = u.unicode 
+                                          WHERE u.level = 'Dosen' AND u.user_id = '$id'";
+                            }
                             // Mengambil id dengan menggunakan session username
                             // $username = $_SESSION['username'];
                             // $id = "SELECT user_id FROM user where username='$username'";
@@ -53,23 +63,27 @@ tr{
                             $result = mysqli_query($koneksi, $query);
                             while ($row = mysqli_fetch_assoc($result)) {
                             ?>
-                                    <strong>Nama: </strong><?= $row['nama'] ?><br><hr>
-                                    <strong>NIP: </strong><?= $row['nip'] ?><br><hr>
-                                    <strong>Username: </strong><?= $row['username'] ?><br><hr>
-                                    <strong>Email: </strong><?= $row['email'] ?><br><hr>
-                                    <strong>Jenis Kelamin: </strong><?= $row['jk'] ?><br><hr>
+                                <strong>Nama: </strong><?= $row['nama'] ?><br><hr>
+                                <?php if ($userLevel === 'Mahasiswa') : ?>
+                                    <strong>NIM: </strong><?= $row['nim'] ?><br><hr>
+                                <?php elseif ($userLevel === 'Dosen') : ?>
+                                    <strong>NIDN: </strong><?= $row['nidn'] ?><br><hr>
+                                <?php endif; ?>
+                                <strong>Username: </strong><?= $row['username'] ?><br><hr>
+                                <strong>Email: </strong><?= $row['email'] ?><br><hr>
+                                <strong>Jenis Kelamin: </strong><?= $row['jk'] ?><br><hr>
 
-                                <?php } ?>
-                                <div class="row">
-                                    <div class="col-sm-12">
-                                        <div class="card">
-                                            <div class="card-body" style="text-align: center;">
-                                            <a href="index.php?page=akun/edit" class="btn btn-warning btn-xs"><i class="fa fa-pencil-square" aria-hidden="true"></i> Edit</a>
+                            <?php } ?>
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <div class="card">
+                                        <div class="card-body" style="text-align: center;">
+                                        <a href="index.php?page=akun/edit" class="btn btn-warning btn-xs"><i class="fa fa-pencil-square" aria-hidden="true"></i> Edit</a>
 
-                                            </div>
                                         </div>
                                     </div>
-                               </div>
+                                </div>
+                            </div>
                        </tbody>
                 </div>
         </main>
