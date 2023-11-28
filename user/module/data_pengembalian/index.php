8685f8932fb15b89083a408fcbb970c52b1387e5
hbbspace <habibatulmustofa1@gmail.com>
2023-11-28 16:25:06 +0700

    Update user dikit

--- user/module/data_pengembalian/index.php
+++ user/module/data_pengembalian/index.php
@@ -1,7 +1,7 @@
 <div class="container-fluid">
     <div class="row">
         <?php
-        include "admin/template/menu.php";
+        include "user/template/menu.php";
         ?>
         <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
             <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
