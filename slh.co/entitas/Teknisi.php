c5b383e217b0e5f12f47c92a373ac5b26bc42163
FajarBayuKusuma <114029998+FajarBayuKusuma@users.noreply.github.com>
2023-11-30 09:34:57 +0700

    Class teknisi

---
+++ slh.co/entitas/Teknisi.php
@@ -0,0 +1,40 @@
+class Teknisi {
+    private $nip;
+    private $namaTeknisi;
+    private $jenisKelamin;
+
+    public function __construct($nip, $namaTeknisi, $jenisKelamin) {
+        $this->nip = $nip;
+        $this->namaTeknisi = $namaTeknisi;
+        $this->jenisKelamin = $jenisKelamin;
+    }
+
+    // Getter dan setter untuk setiap atribut
+    public function getNip() {
+        return $this->nip;
+    }
+
+    public function setNip($nip) {
+        $this->nip = $nip;
+    }
+
+    public function getNamaTeknisi() {
+        return $this->namaTeknisi;
+    }
+
+    public function setNamaTeknisi($namaTeknisi) {
+        $this->namaTeknisi = $namaTeknisi;
+    }
+
+    public function getJenisKelamin() {
+        return $this->jenisKelamin;
+    }
+
+    public function setJenisKelamin($jenisKelamin) {
+        $this->jenisKelamin = $jenisKelamin;
+    }
+
+    public function __toString() {
+        return "Teknisi { nip='" . $this->nip . "', nama_teknisi='" . $this->namaTeknisi . "', jk=" . $this->jenisKelamin . " }";
+    }
+}
