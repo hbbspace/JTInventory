c149c1643e5d2376fa1e729df3789c9a554ebca4
hbbspace <habibatulmustofa1@gmail.com>
2023-12-07 11:36:43 +0700

    Reparasi Admin Berlanjut

---
+++ slh.co/app/models/Login_Model.php
@@ -0,0 +1,26 @@
+<?php
+
+class Login_Model {
+	
+	private $db;
+
+	public function __construct()
+	{
+		$this->db = new Database;
+	}
+
+	public function checkLogin($username, $password) {
+		//$username = antiinjection($koneksi, $_POST['username']);
+        // $password = antiinjection($koneksi, $_POST['password']);
+
+        $this->db->query("SELECT user_id,unicode, username, level, salt, password as hashed_password FROM user WHERE username = :$username;");
+		$this->db->bind('username', $username);
+		$this->db->bind('password', md5($password));
+		return $this->db->resultSet();
+		
+		//$this->db->execute();
+		//return $this->db->rowCount();
+		// $data =  $this->db->single();
+		// return $data;
+	}
+}
