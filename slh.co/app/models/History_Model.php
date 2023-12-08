e3cdb4257bc7bf8ab8c33a65a30e37e4b6b84ac7
hbbspace <habibatulmustofa1@gmail.com>
2023-12-08 20:16:03 +0700

    sedikit membenahi history

--- slh.co/app/models/History_Model.php
+++ slh.co/app/models/History_Model.php
@@ -7,12 +7,12 @@ class History_Model extends Controller {
         $this->db=new Database;
     }
     public function getAllhistory(){
-        $querry="SELECT m.nama_mhs AS nama, p.time AS waktu, u.level AS status, p.id_peminjaman AS id FROM peminjaman AS p
+        $querry="SELECT m.nama_mhs AS nama, p.time AS waktu, u.level AS status, p.id_peminjaman AS id, p.keterangan AS keterangan FROM peminjaman AS p
         INNER JOIN user AS u ON u.user_id = p.user_id
         INNER JOIN mahasiswa AS m ON m.nim = u.unicode
         WHERE p.status = 'done' OR p.status ='failed'
         UNION
-        SELECT d.nama_dosen AS nama, p.time AS waktu, u.level AS status, p.id_peminjaman AS id FROM peminjaman AS p
+        SELECT d.nama_dosen AS nama, p.time AS waktu, u.level AS status, p.id_peminjaman AS id, p.keterangan AS keterangan FROM peminjaman AS p
         INNER JOIN user AS u ON u.user_id = p.user_id
         INNER JOIN dosen AS d ON d.nidn = u.unicode
         WHERE p.status = 'done' OR p.status ='failed'";
