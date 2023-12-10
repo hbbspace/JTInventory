70a7ee057d959f2c8dc262ab4f4bf34e6e661abf
FajarBayuKusuma <114029998+FajarBayuKusuma@users.noreply.github.com>
2023-12-10 22:50:48 +0700

    Admin cuyy

--- slh.co/app/models/Admin.php
+++ slh.co/app/models/Admin.php
@@ -1,10 +1,64 @@
 <?php
 
-class Admin extends Aktor {
+abstract class Admin extends Aktor {
 
     // public function user($userId, $unicode, $email, $username, $password, $salt, $level) {
 
     // }
+    private $db;
+
+    public function __construct(){
+        $this->db=new Database;
+    }
+    public function TampilDataBarang(){
+        $query="SELECT* FROM barang";
+        $this->db->query($query);
+        return $this->db->resultSet();
+    }
+    public function TampilPeminjaman(){
+        $querry="SELECT m.nama_mhs AS nama, p.time AS waktu, u.level AS status, p.id_peminjaman AS id FROM peminjaman AS p
+        INNER JOIN user AS u ON u.user_id = p.user_id
+        INNER JOIN mahasiswa AS m ON m.nim = u.unicode
+        WHERE p.status = 'request'
+        UNION
+        SELECT d.nama_dosen AS nama, p.time AS waktu, u.level AS status, p.id_peminjaman AS id FROM peminjaman AS p
+        INNER JOIN user AS u ON u.user_id = p.user_id
+        INNER JOIN dosen AS d ON d.nidn = u.unicode
+        WHERE p.status = 'request'";
+        $this->db->query($querry);
+
+        return $this->db->resultSet();
+    }
+    public function TampilPengembalian(){
+        $querry="SELECT m.nama_mhs AS nama, p.time AS waktu, u.level AS status, p.id_peminjaman AS id FROM peminjaman AS p
+        INNER JOIN user AS u ON u.user_id = p.user_id
+        INNER JOIN mahasiswa AS m ON m.nim = u.unicode
+        WHERE p.status = 'progress'
+        UNION
+        SELECT d.nama_dosen AS nama, p.time AS waktu, u.level AS status, p.id_peminjaman AS id FROM peminjaman AS p
+        INNER JOIN user AS u ON u.user_id = p.user_id
+        INNER JOIN dosen AS d ON d.nidn = u.unicode
+        WHERE p.status = 'progress'";
+        $this->db->query($querry);
+        return $this->db->resultSet();
+    }
+    public function TampilAkun(){
+        
+    }
+    public function TampilHistory(){
+        $querry="SELECT m.nama_mhs AS nama, p.time AS waktu, u.level AS status, p.id_peminjaman AS id, p.keterangan as keterangan FROM peminjaman AS p
+        INNER JOIN user AS u ON u.user_id = p.user_id
+        INNER JOIN mahasiswa AS m ON m.nim = u.unicode
+        WHERE p.status = 'done' OR p.status ='failed'
+        UNION
+        SELECT d.nama_dosen AS nama, p.time AS waktu, u.level AS status, p.id_peminjaman AS id, p.keterangan as keterangan FROM peminjaman AS p
+        INNER JOIN user AS u ON u.user_id = p.user_id
+        INNER JOIN dosen AS d ON d.nidn = u.unicode
+        WHERE p.status = 'done' OR p.status ='failed'";
+        $this->db->query($querry);
+
+        return $this->db->resultSet();
+    }
 
     public function index() {
         $data['title'] = 'Home';
