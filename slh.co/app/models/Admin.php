5cd5dfb2b74260d7b7b05127388683a9e077dc1d
FajarBayuKusuma <114029998+FajarBayuKusuma@users.noreply.github.com>
2023-12-10 22:27:27 +0700

    Class Aktor guyss

---
+++ slh.co/app/models/Admin.php
@@ -0,0 +1,95 @@
+<?php
+
+class Admin extends Aktor {
+
+    // public function user($userId, $unicode, $email, $username, $password, $salt, $level) {
+
+    // }
+
+    public function index() {
+        $data['title'] = 'Home';
+        $data['admin']=$this->model('Admin_model')->getAllAdmin();
+
+        $this->view('templates/top');
+        $this->view('templates/sideMenuAdmin');
+        $this->view('admin/index', $data);
+        $this->view('templates/bottom');
+    }
+
+    public function tambahAdmin() {
+        if($this->model('Admin_Model')->tambahDataAdmin($_POST) > 0) {
+            Flasher::setMessage('Berhasil','Ditambahkan','success');
+            header('Location: ' . base_url . '/Admin');
+            exit; 
+        }else{
+            Flasher::setMessage('Gagal','Ditambahkan','danger');
+            header('Location: ' . base_url . '/Admin');
+            exit; 
+
+        }
+    }
+    public function hapusAdmin($nip) {
+        if($this->model('Admin_model')->hapusDataAdmin($nip) > 0) {
+            Flasher::setMessage('Berhasil','Dihapus','success');
+            header('Location: ' . base_url . '/Admin');
+            exit; 
+        }else{
+            Flasher::setMessage('Gagal','Dihapus','danger');
+            header('Location: ' . base_url . '/Admin');
+            exit; 
+        }
+    }
+
+    // public function Data_Barang() {
+    //     $data['title'] = 'Data Barang';
+    //     $data['barang']=$this->model('Admin_model')->getAllBarang();
+
+    //     $this->view('templates/top');
+    //     $this->view('list_barang/index', $data);
+    //     $this->view('templates/bottom');
+    // }
+
+    // public function Data_Peminjaman() {
+    //     $data['title'] = 'Data Peminjaman';
+    //     $data['peminjaman']=$this->model('Admin_model')->getAllPeminjaman();
+
+    //     $this->view('templates/top');
+    //     $this->view('data_peminjaman/index', $data);
+    //     $this->view('templates/bottom');
+    // }
+
+    // public function Data_Pengembalian() {
+    //     $data['title'] = 'Data Peminjaman';
+    //     $data['admin']=$this->model('Admin_model')->getAllPeminjaman();
+
+    //     $this->view('templates/top');
+    //     $this->view('data_pengembalian/index', $data);
+    //     $this->view('templates/bottom');
+    // }
+
+    // public function History() {
+    //     $data['title'] = 'History';
+    //     $data['admin']=$this->model('Admin_model')->getAllAdmin();
+
+    //     $this->view('templates/top');
+    //     $this->view('history/index', $data);
+    //     $this->view('templates/bottom');
+    // }
+
+    // public function Akun() {
+    //     $data['title'] = 'Akun';
+    //     $data['admin']=$this->model('Admin_model')->getAllAdmin();
+
+    //     $this->view('templates/top');
+    //     $this->view('akun/index', $data);
+    //     $this->view('templates/bottom');
+    // }
+
+    // public function Logout() {
+    //     //$data['admin']=$this->model('Admin_model')->getAllAdmin();
+
+    //     $data['title'] = 'Halaman Login';
+
+	// 	$this->view('login/index', $data);
+    // }
+}
--- slh.co/app/models/Aktor.php
+++ slh.co/app/models/Aktor.php
@@ -1,6 +1,6 @@
 <?php
 // Abstract class Aktor
-abstract class Aktor extends Controller {
+abstract class Aktor{
     protected $userId;
     protected $unicode;
     protected $email;
@@ -9,18 +9,35 @@ abstract class Aktor extends Controller {
     protected $salt;
     protected $level;
 
-    
-    public function __construct($userId, $unicode, $email, $username, $password, $salt, $level) {
-        $this->userId = $userId;
-        $this->unicode = $unicode;
-        $this->email = $email;
-        $this->username = $username;
-        $this->password = $password;
-        $this->salt = $salt;
-        $this->level = $level;
+    private $db;
+
+	public function __construct()
+	{
+		$this->db = new Database;
+	}
+
+    public function LogIn($data){
+		$query = "SELECT user_id, unicode, username, level, salt, password as hashed_password FROM user WHERE username = :username";
+		$this->db->query($query);
+		$this->db->bind('username', $data['username']);
+		$result = $this->db->single();        
+        $userId=$result['user_id'];
+        $unicode=$result ['result'];
+        $email =$result['email'];
+        $username =$result ['username'];
+        $password =$result ['password'];
+        $salt=$result ['salt'];
+        $level =$result ['level'];
     }
 
-    public abstract function user($userId, $unicode, $email, $username, $password, $salt, $level);
+    public abstract function TampilDataBarang();
+    public abstract function TampilPeminjaman();
+    public abstract function TampilPengembalian();
+    public abstract function TampilAkun();
+    public abstract function TampilHistory();
+
+
+    // public abstract function user($userId, $unicode, $email, $username, $password, $salt, $level);
 
     public function getuserId() {
         return $this->userId;
