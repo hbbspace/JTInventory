e1ab9e030c4d790dc0365c3faa0971eeed7b24d9
RifkiSstwnn <rifkincorp@gmail.com>
2023-12-18 11:36:48 +0700

    pengecekan unicode pada register agar tidak ada duplikasi

--- slh.co/app/models/Helper.php
+++ slh.co/app/models/Helper.php
@@ -66,40 +66,52 @@ class Helper {
             $data['password'] != null &&
             $data['level'] != "Pilih Level"
         ) {
-            $ambilId = "SELECT user_id FROM user ORDER BY user_id DESC LIMIT 1";
-            $this->db->query($ambilId);
-            $id = $this->db->single();
-            $newUserId = $id['user_id'] + 1;
-            $query = "INSERT INTO user VALUES (:newUserId, :unicode, :email, :username, :password, :salt, :level)";
-    
+            $unicode=$data['unicode'];
+            $query="SELECT * FROM user WHERE unicode=:unicode";
             $this->db->query($query);
-            $this->db->bind('newUserId', $newUserId);
-            $this->db->bind('unicode', $data['unicode']);
-            $this->db->bind('email', $data['email']);
-            $this->db->bind('username', $data['username']);
-            $this->db->bind('password', $data['password']); // Ubah ke hashed_password
-            $this->db->bind('salt', $salt);
-            $this->db->bind('level', $data['level']);
-            $this->db->execute();
-            if($data['level']=='Mahasiswa'){
-                $query2 = "INSERT INTO mahasiswa VALUES (:nim, :nama_mhs, :jk)";
-                $this->db->query($query2);
-                $this->db->bind('nim', $data['unicode']);
-                $this->db->bind('nama_mhs', $data['nama']);
-                $this->db->bind('jk', $data['jenis_kelamin']);
-                $this->db->execute();
-                return 1;
-            }else if($data['level']=='Dosen'){
-                $query2 = "INSERT INTO dosen VALUES (:nidn, :nama_dosen, :jk)";
-                $this->db->query($query2);
-                $this->db->bind('nidn', $data['unicode']);
-                $this->db->bind('nama_dosen', $data['nama']);
-                $this->db->bind('jk', $data['jenis_kelamin']);
-                $this->db->execute();
-                return 1;
-            }else{
-                Flasher::setMessage('Gagal', 'Registrasi Karena data tidak Valid', 'danger');
+            $this->db->bind(':unicode', $unicode);
+            // Menjalankan query dan mendapatkan hasilnya
+            $result = $this->db->single();
+            if($result){
+                Flasher::setMessage('Gagal', 'Registrasi Karena Unicode Sudah Digunakan', 'danger');
                 return 0;
+            }else{
+
+                $ambilId = "SELECT user_id FROM user ORDER BY user_id DESC LIMIT 1";
+                $this->db->query($ambilId);
+                $id = $this->db->single();
+                $newUserId = $id['user_id'] + 1;
+                $query = "INSERT INTO user VALUES (:newUserId, :unicode, :email, :username, :password, :salt, :level)";
+        
+                $this->db->query($query);
+                $this->db->bind('newUserId', $newUserId);
+                $this->db->bind('unicode', $data['unicode']);
+                $this->db->bind('email', $data['email']);
+                $this->db->bind('username', $data['username']);
+                $this->db->bind('password', $data['password']); // Ubah ke hashed_password
+                $this->db->bind('salt', $salt);
+                $this->db->bind('level', $data['level']);
+                $this->db->execute();
+                if($data['level']=='Mahasiswa'){
+                    $query2 = "INSERT INTO mahasiswa VALUES (:nim, :nama_mhs, :jk)";
+                    $this->db->query($query2);
+                    $this->db->bind('nim', $data['unicode']);
+                    $this->db->bind('nama_mhs', $data['nama']);
+                    $this->db->bind('jk', $data['jenis_kelamin']);
+                    $this->db->execute();
+                    return 1;
+                }else if($data['level']=='Dosen'){
+                    $query2 = "INSERT INTO dosen VALUES (:nidn, :nama_dosen, :jk)";
+                    $this->db->query($query2);
+                    $this->db->bind('nidn', $data['unicode']);
+                    $this->db->bind('nama_dosen', $data['nama']);
+                    $this->db->bind('jk', $data['jenis_kelamin']);
+                    $this->db->execute();
+                    return 1;
+                }else{
+                    Flasher::setMessage('Gagal', 'Registrasi Karena data tidak Valid', 'danger');
+                    return 0;
+                }
             }
         } else {
             return 0;
